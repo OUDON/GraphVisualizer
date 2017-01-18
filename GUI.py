1532888696c624066044c56a750bdc9a62004e47
T.Tomita <ttp8101@gmail.com>
2017-01-18 16:28:44 +0900

    Use VisualizableGraph as QGraphicItem

--- GUI.py
+++ GUI.py
@@ -15,23 +15,22 @@ class MainWindow(QtGui.QWidget):
         self.scene = QtGui.QGraphicsScene(self)
         self.scene.setSceneRect(0, 0, 500, 500)
         self.ui.graphicsView.setScene(self.scene)
-        self.graphView = GraphView(500, 500)
-        self.scene.addItem(self.graphView)
+
+        self.graph = VisualizableGraph()
+        self.scene.addItem(self.graph)
 
         self.ui.button_draw.clicked.connect(self.loadGraph)
-        self.initGraphView()
+        self.initGraph()
 
-    def initGraphView(self):
-        self.graph = VisualizableGraph()
+    def initGraph(self):
         self.graph.load_from_file('data/graph4.txt')
-        self.graphView.setGraph(self.graph)
 
     def loadGraph(self):
         graph_str = self.ui.text_graph.toPlainText()
         directed  = self.ui.radiobtn_directed.isChecked()
         weighted  = self.ui.checkbox_weighted.isChecked()
         self.graph.load(graph_str, directed, weighted)
-        self.graphView.setGraph(self.graph)
+        # self.graphView.setGraph(self.graph)
 
 def main():
     app = QtGui.QApplication(sys.argv)
--- Graph.py
+++ Graph.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+from PyQt4 import QtGui, QtCore
 
 import argparse
 import sys
@@ -12,12 +14,17 @@ TIME_STEP           = 0.5
 DAMPING_COEFFICIENT = 0.8
 
 class Edge:
-    def __init__(self, to, weight, edge_type):
-        self.to        = to
-        self.weight    = weight
-        self.edge_type = edge_type
+    def __init__(self, frm, to, weight, edge_type):
+        self.frm               = frm
+        self.to                = to
+        self.weight            = weight
+        self.normalized_weight = 1
+        self.edge_type         = edge_type
 
-class Graph:
+    def weight_normalize(self, max_weight):
+        self.normalized_weight = self.weight / max_weight
+
+class Graph(object):    
     def __init__(self):
         self.N, self.M = 0, 0
         self.weighted  = False
@@ -39,11 +46,11 @@ class Graph:
             weight = int(splitted_line[2]) if weighted else 1
 
             if self.directed:
-                self.graph[u].append(Edge(v, weight, 'directed'))
-                self.graph[v].append(Edge(u, weight, 'directed-inverse'))
+                self.graph[u].append(Edge(u, v, weight, 'directed'))
+                self.graph[v].append(Edge(v, u, weight, 'directed-inverse'))
             else:
-                self.graph[u].append(Edge(v, weight, 'undirected'))
-                self.graph[v].append(Edge(u, weight, 'undirected'))
+                self.graph[u].append(Edge(u, v, weight, 'undirected'))
+                self.graph[v].append(Edge(v, u, weight, 'undirected'))
         if weighted: self.__weight_normalize()
 
     def load_from_file(self, file_name, directed=False, weighted=False):
@@ -66,27 +73,30 @@ class Graph:
                 max_weight = max(max_weight, edge.weight)
         for u in range(self.N):
             for edge in self.graph[u]:
-                edge.weight /= max_weight
+                edge.weight_normalize(max_weight)
 
-class VisualizableGraph(Graph):
-    def __init__(self):
+class VisualizableGraph(QtGui.QGraphicsItem, Graph):
+    def __init__(self, width=500, height=500, mergin=50):
+        super(VisualizableGraph, self).__init__()
         Graph.__init__(self)
+
         self.pos = []
+        self.width = width
+        self.height = height
+        self.mergin = mergin
 
     def load(self, graph_str, directed=False, weighted=False):
         Graph.load(self, graph_str, directed, weighted)
         self.compute_node_position()
+        self.resize_graph()
+        self.update()
 
-    def show(self):
-        plt.plot(self.pos.T[0], self.pos.T[1], 'o')
-        for u in range(len(self.graph)):
-            for edge in self.graph[u]:
-                v = edge.to
-                plt.plot([self.pos[u][0], self.pos[v][0]],
-                         [self.pos[u][1], self.pos[v][1]])
-        plt.show()
+    def resize_graph(self):
+        w = self.width - self.mergin*2
+        h = self.height - self.mergin*2
+        self.__position_normalize(w, h, self.mergin)
 
-    def position_normalize(self, width, height, mergin):
+    def __position_normalize(self, width, height, mergin):
         x_range = np.amax(self.pos.T[0]) - np.amin(self.pos.T[0])
         x_min   = np.amin(self.pos.T[0])
         y_range = np.amax(self.pos.T[1]) - np.amin(self.pos.T[1])
@@ -136,8 +146,8 @@ class VisualizableGraph(Graph):
             dy = self.pos[v][1] - self.pos[u][1]
 
             l = np.sqrt(dx * dx + dy * dy) + 1e-5
-            springLengthX = NATURAL_LENGTH_MAX * edge.weight * dx / l
-            springLengthY = NATURAL_LENGTH_MAX * edge.weight * dy / l
+            springLengthX = NATURAL_LENGTH_MAX * edge.normalized_weight * dx / l
+            springLengthY = NATURAL_LENGTH_MAX * edge.normalized_weight * dy / l
 
             F[0] += SPRING_CONSTANT * (dx - springLengthX)
             F[1] += SPRING_CONSTANT * (dy - springLengthY)
@@ -151,6 +161,72 @@ class VisualizableGraph(Graph):
         for i in range(self.N):
             self.pos[i][0] -= gx
             self.pos[i][1] -= gy
+
+    # Methods for QtGui.QGraphicsItem
+    def paint(self, painter, option, widget):
+        self.__draw_graph(painter)
+
+    def __draw_graph(self, painter):
+        G = self.graph
+        pen = QtGui.QPen(QtCore.Qt.black, 1, QtCore.Qt.SolidLine)
+        painter.setPen(pen)
+        painter.setBrush(QtCore.Qt.white)
+
+        for u in range(self.N):
+          for edge in G[u]:
+              self.__draw_edge(edge, painter)
+
+        #"""
+        # node
+        for u in range(self.N):
+            x, y = self.pos[u][0], self.pos[u][1]
+            painter.drawEllipse(QtCore.QPointF(x, y), 20, 20)
+
+        # label
+        painter.setFont(QtGui.QFont('Helvetica', 24))
+        for u in range(self.N):
+            x, y = self.pos[u][0], self.pos[u][1]
+            painter.drawText(x-20, y-20, 40, 40, QtCore.Qt.AlignCenter, str(u+1))
+        #"""
+
+    def __draw_edge(self, edge, painter):
+        u, v = edge.frm, edge.to
+        # f, t = self.pos[u], self.pos[v]
+        tmp_f, tmp_t = self.pos[u], self.pos[v]
+        direction = (tmp_t - tmp_f) / np.linalg.norm(tmp_t - tmp_f)
+        f = tmp_f + direction * 20
+        t = tmp_t - direction * 20
+
+        dx = (t - f)[0]
+        dy = (t - f)[1]
+        length = np.linalg.norm(t - f)
+
+        if edge.edge_type == 'undirected':
+            painter.drawLine(f[0], f[1], t[0], t[1])
+
+        elif edge.edge_type == 'directed':
+            angle = math.acos(dx / length)
+            if dy >= 0:
+                angle = 2 * math.pi - angle
+            v1 = t + np.array([
+                math.sin(angle - math.pi / 3) * 15,
+                math.cos(angle - math.pi / 3) * 15
+            ])
+
+            v2 = t + np.array([
+                math.sin(angle - math.pi * 2 / 3) * 15,
+                math.cos(angle - math.pi * 2 / 3) * 15
+            ])
+
+            painter.drawLine(f[0], f[1], t[0], t[1])
+            painter.drawLine(v1[0], v1[1], t[0], t[1])
+            painter.drawLine(v2[0], v2[1], t[0], t[1])
+
+        elif edge.edge_type == 'directed-inverse':
+            pass
+
+    def boundingRect(self):
+        return QtCore.QRectF(0, 0, self.width, self.height)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
